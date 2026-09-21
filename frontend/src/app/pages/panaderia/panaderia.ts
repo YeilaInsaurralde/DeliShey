f77acf31aa30f7a06bcb613020223b537ca36337
YeilaInsaurralde <yeilainsaurralde@gmail.com>
2026-09-20 21:55:55 -0300

    chore: borrar componentes y paginas sin uso y package-lock vacio de la raiz

--- frontend/src/app/pages/panaderia/panaderia.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-panaderia',
-  imports: [],
-  templateUrl: './panaderia.html',
-  styleUrl: './panaderia.scss',
-})
-export class Panaderia {}
